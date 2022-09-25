18edeecbd58da77cd8bc9acf2e964d3a599dffac
markcaf <marcocafassog@gmail.com>
2022-09-25 22:49:17 +0200

    feat(seeds): add tags table seeder

---
+++ database/seeds/TagsTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Tag;
+use Illuminate\Database\Seeder;
+
+class TagsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tags = [
+            '#coding',
+            '#development',
+            '#style',
+            '#backend',
+            '#frontend',
+            '#news',
+            '#framework',
+            '#feature',
+            '#learning',
+        ];
+
+        foreach ($tags as $tag){
+            $newTag = new Tag();
+            $newTag->name = $tag;
+            $newTag->save();
+        }
+    }
+}
